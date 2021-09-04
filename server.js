3f333e30d0c7109edcb066fcb119ed33b1ca0def
Mustafa Ali <mustafamire@gmail.com>
2021-09-04 19:01:24 +0100

    Add: variable for server & express

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ const express = require("express");
 const path = require("path");
 const apiRouter = require("./routes/api.js");
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
+
