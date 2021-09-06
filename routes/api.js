ef61745d9d78d7f35bf8cbf21169796656221750
Mustafa Ali <mustafamire@gmail.com>
2021-09-06 11:36:49 +0100

    Add: Declare routes for API requests

--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,5 @@
-console.log("hello world");
+//Routing for our API requests//
+const api = require("express").Router();
+const fs = require("fs");
+// Id generator 
+const { v4: uuidv4 } = require("uuid");
