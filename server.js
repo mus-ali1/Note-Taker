3dea5a170f4fdf442774d9653512ace761c95d43
Mustafa Ali <mustafamire@gmail.com>
2021-09-06 10:58:57 +0100

    Add: app.use/ app.get/app.listen

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const apiRouter = require("./routes/api.js");
+const apiRouter = require("./routes.api.js");
 
 
 // Set the environment variable PORT to tell your web server what port to listen on, also set variable for experess for use later on.
@@ -11,4 +11,27 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+
+
+// Sets up route which forwards all API requests
+app.use("/api/notes", apiRouter);
+
+
+//Return notes.html
+app.get("/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+//Returns index.html for any other requests 
+app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "public/index.html"))
+)
+
+
+// Ensures server is listening for requests at the correct port
+app.listen(PORT, () =>
+    console.log(`Listening for requests on port ${PORT}.`)
+);
+
 
