5dc9facc8b9b21aad25d5f34a4d8c38d09cc72f0
Mustafa Ali <mustafamire@gmail.com>
2021-09-06 11:39:51 +0100

    Add: Declare get request from our server + file name change

--- routes/api.js
+++ routes/api.js
@@ -3,3 +3,19 @@ const api = require("express").Router();
 const fs = require("fs");
 // Id generator 
 const { v4: uuidv4 } = require("uuid");
+
+
+
+// Declare .get request from our server
+api.get("/", (req, res) => {
+    console.log(`${req.method} request received.`);
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(404).send("Notes not found").end();
+        }
+
+        res.json(JSON.parse(data));
+    })
+})
