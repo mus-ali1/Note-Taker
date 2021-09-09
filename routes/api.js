4301ad63f034e3eb168e39e9949eebdf6d9a12ed
Mustafa Ali <mustafamire@gmail.com>
2021-09-09 14:00:31 +0100

    Fix: Add __dirname to file path

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,7 @@ console.log("dirname: ", __dirname)
 api.get("/", (req, res) => {
     console.log(`${req.method} request received.`);
 
-    fs.readFile(path.join("../db/db.json"), "utf8", (err, data) => {
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
 
         if (err) {
             console.error(err);
@@ -44,7 +44,7 @@ api.post("/", (req, res) => {
 
         // Update our ReadFile and send data back as Json and then update db.json
 
-        fs.readFile(path.join("../db/db.json"), "utf8", (err, data) => {
+        fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
             if (err) {
                 console.error(err)
             } else {
@@ -55,7 +55,7 @@ api.post("/", (req, res) => {
                 res.json(JSON.parse(data));
 
 
-                fs.writeFile(path.join("../db/db.json"), JSON.stringify(notesArray, null, 4), err =>
+                fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notesArray, null, 4), err =>
                     err ? console.error(err) : console.log("Data written to file.")
                 )
             }
@@ -69,7 +69,7 @@ api.post("/", (req, res) => {
 
 api.delete("/:id", (req, res) => {
 
-    fs.readFile(path.join("../db/db.json"), "utf8", (err, data) => {
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
         if (err) {
             console.error(err);
             res.status(404).send("Notes not found").end();
@@ -83,7 +83,7 @@ api.delete("/:id", (req, res) => {
         res.json(updatedNotes);
 
 
-        fs.writeFile(path.join("../db/db.json"), JSON.stringify(updatedNotes, null, 4), err =>
+        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(updatedNotes, null, 4), err =>
             err ? console.error(err) : console.log("Note deleted from file.")
         )
 
